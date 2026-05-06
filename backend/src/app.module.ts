397771cdbc2ed616974f71c1d13946b11486c8cc
Leoauchana <matiasleonelcja@gmail.com>
2026-05-06 01:12:44 -0300

    chore: Se instalo el paquete schedule de nestjs para la automatizacion del proceso de bares

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { SyncModule } from './sync/sync.module';
 import { VenuesModule } from './venues/venues.module';
 @Module({
-  imports: [VenuesModule],
+  imports: [VenuesModule, SyncModule],
 })
 export class AppModule {}
