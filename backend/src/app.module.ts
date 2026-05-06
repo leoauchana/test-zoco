ccb7d5a5970adca82dc69df427be076607ff9421
Leoauchana <matiasleonelcja@gmail.com>
2026-05-06 14:12:32 -0300

    chore: Se instalaron y desinstalaron dependencias del proyecto y se modificaron archivos de configuracion

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { LogsModule } from './logs/logs.module';
 import { SyncModule } from './sync/sync.module';
 import { VenuesModule } from './venues/venues.module';
 @Module({
-  imports: [VenuesModule, SyncModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    VenuesModule,
+    SyncModule,
+    LogsModule,
+  ],
 })
 export class AppModule {}
